f9b9f769bc1300926f867956e06e5e4f7112c743
Abu Osama <112826965+Abu1Osama@users.noreply.github.com>
2024-09-15 11:05:08 +0530

    booklibrary restapi

---
+++ src/main/java/com/abuosama/learningspringboot/controller/BookController.java
@@ -0,0 +1,39 @@
+package com.abuosama.learningspringboot.controller;
+
+import com.abuosama.learningspringboot.model.Book;
+import com.abuosama.learningspringboot.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Optional<Book> book = bookService.getBookById(id);
+        return book.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        return bookService.addBook(book);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+    }
+}
